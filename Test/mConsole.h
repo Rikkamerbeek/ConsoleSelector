e84090c7ea8730b50258c32d8f9ea165a21ff3c8
kilydan <tim.hoenselaar@gmail.com>
2018-11-05 14:29:03 +0100

    filled mConsole.h

--- Test/mConsole.h
+++ Test/mConsole.h
@@ -8,7 +8,10 @@
 class mConsole : public iConsole
 {
 public:
-
+    MOCK_METHOD0(TurnOn(), void());
+    MOCK_METHOD1(TurnOff(), void());
+    MOCK_METHOD2(GetPos(), int());
+    MOCK_METHOD3(GetName(), std::string());
 };
 
 #endif
