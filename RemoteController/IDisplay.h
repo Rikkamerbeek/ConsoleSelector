25277d9a0902f47fabb1e6345fd0ed6a12f0fedc
rm.k@live.nl <rm.k@live.nl>
2018-11-05 14:13:22 +0100

    added good file names

--- RemoteController/IDisplay.h
+++ RemoteController/IDisplay.h
@@ -1,10 +1,17 @@
 #ifndef IDISPLAY_H
 #define IDISPLAY_H
 
+#include "IConsole.h"
+
 class iDisplay 
 {
 public:
+    virtual ~iDisplay() {};
 
-private:
+    virtual void SetConsoles(Console consoles) = 0;
+    virtual void DisplayOff() = 0;
+    virtual void GetCurrentPosition(int pos) = 0;
 
-}
+};
+
+#endif
