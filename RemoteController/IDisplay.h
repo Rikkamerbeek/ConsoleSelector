bfcbb9569c18f8539e460125b618975a22c86049
rm.k@live.nl <rm.k@live.nl>
2018-11-07 11:00:28 +0100

    some Errors handled

--- RemoteController/IDisplay.h
+++ RemoteController/IDisplay.h
@@ -1,14 +1,14 @@
 #ifndef IDISPLAY_H
 #define IDISPLAY_H
 
-
+#include <vector>
 
 class iDisplay 
 {
 public:
     virtual ~iDisplay() {};
 
-    virtual void SetConsoles(vector<string> consoles) = 0;
+    virtual void SetConsoles(const std::vector<string>& consoles) = 0;
     virtual void DisplayOff() = 0;
     virtual void GetCurrentPosition(int pos) = 0;
 
--- RemoteController/RemoteController.cpp
+++ RemoteController/RemoteController.cpp
@@ -11,8 +11,56 @@ Remotecontroller::~Remotecontroller()
 {
 }
 
-States HandleIdleState(Events ev);
-States HandleSelectingState(Events ev);
-States HandleSwitchingState(Events ev);
-States HandleAddingState(Events ev);
-States HandleRunningState(Events ev);
+States HandleIdleState(Events ev)
+{
+    States result = STATE_IDLE;
+    return result;
+}
+
+States HandleSelectingState(Events ev)
+{
+    States result = STATE_SELECTING;
+    return result;
+}
+
+States HandleSwitchingState(Events ev)
+{
+    States result = STATE_SWITCHING;
+    return result;
+} 
+
+States HandleAddingState(Events ev)
+{
+    States result = STATE_ADDING;
+    return result;
+}
+
+States HandleRunningState(Events ev)
+{
+    States result = STATE_RUNNING;
+    return result;
+}
+
+void Remotecontroller::HandleEvent(Events ev)
+{
+    switch (currentState)
+    {
+    case STATE_STANDBY:
+        currentstate = HandleIdleState(ev);
+        break;
+    case STATE_SELECTING:
+        currentState = HandleSelectingState(ev);
+        break;
+    case STATE_CHANGING:
+        currentstate = HandleSwitchingState(ev);
+        break;
+    case STATE_RUNNING:
+        currentState = HandleRunningState(ev);
+        break;
+    case STATE_ADDING:
+        currentstate = HandleAddingState(ev);
+        break;
+    default:
+        break;
+    }
+}
